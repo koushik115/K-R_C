94b4ecc29c4a3d51988847add2e1b4006f7296a1
Koushik <kaushikbhat115@gmail.com>
2026-02-17 22:33:27 +0530

    Add RPN calculator to chapterFour.c
    Implement a Reverse Polish (RPN) calculator in chapterFour.c: add infixCalculator(), stack push/pop, getop() (with negative-number handling), getch()/ungetch(), and related macros (MAXOP, MAXVAL, BUFSIZE, NUMBER). The original main logic is commented out and now calls infixCalculator(). Compiled binaries (a.out, chapterFour) were also updated.

--- chapterFour.c
+++ chapterFour.c
@@ -3,14 +3,26 @@
 #include <ctype.h>
 
 #define MAXLINE	1000
+#define MAXOP	100
+#define MAXVAL	100
+#define BUFSIZE	100
+#define NUMBER  '0'
 
 int getLine(char *line, int max);
 int strindex(const char *source, const char *searchfor);
 int strrindex(const char *source, const char *searchfor);
 
+void infixCalculator(void);
+int getop(char *s);
+void push(double f);
+double pop(void);
+
+
+
 char *pattern = "ould";
 
 int main(void) {
+	/*
 	char line[MAXLINE];
 	int found = 0;
 
@@ -22,6 +34,23 @@ int main(void) {
 	}
 
 	return found;
+	*/
+
+	/* Algo for infix (Reverse Polish) calculator */
+	/* while (next operator or opearand is not EOF)
+	 * 	if (number)
+	 * 		push it
+	 * 	else if (operator)
+	 * 		pop operands
+	 * 		do operations
+	 * 		push result
+	 * 	else if (new line)
+	 * 		pop and print the top of the stack
+	 * 	else
+	 * 		error
+	 */
+
+	infixCalculator();
 }
 
 int getLine(char *line, int max) {
@@ -49,6 +78,7 @@ int strindex(const char *source, const char *searchfor) {
 
 	return -1;
 }
+
 
 int strrindex(const char *source, const char *searchfor) {
 	int i, j, k;
@@ -113,9 +143,154 @@ double asciiToFloat(const char *s) {
     
     return sign * val * exponent / power;
 }
+
+void infixCalculator(void) {
+	int type;
+	double op2;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+		case NUMBER:
+			push(asciiToFloat(s));
+			break;
+
+		case '+':
+			push(pop() + pop());
+			break;
+		
+		case '*':
+			push(pop() * pop());
+			break;
+
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+
+		case '/':
+			op2 = pop();
+			if(op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+
+		case '%':
+			op2 = pop();
+			if (op2 != 0.0)
+			    push((int)pop() % (int)op2);
+			else
+			    printf("error: zero divisor\n");
+			break;
+
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+
+		default:
+			printf("error: unknown command %s\n", s);
+			break;
+		}
+	}
+}
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else 
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void) {
+	if (sp > 0)
+		return val[--sp];
+	else 
+		printf("error: stack empty\n");
+
+	return 0.0;
+}
+
+int getch(void);
+void ungetch(int c);
+
+
+// int getop(char *s) {
+// 	int i, c;
+
+// 	while ((s[0] = c = getch()) == ' ' || c == '\t');
+//         s[1] = '\0';
+	
+// 	if (!isdigit(c) && c != '.')
+// 		return c;
+
+// 	i = 0;
+// 	if (isdigit(c))
+// 		while(isdigit(s[++i] = c = getch()));
+	
+// 	if (c == '.')
+// 		while(isdigit(s[++i] = c = getch()));
+
+// 	s[i] = '\0';
+	
+// 	if (c != EOF) 
+// 		ungetch(c);
+
+// 	return NUMBER;
+// }
+
+
+int getop(char *s) {
+    int i = 0, c, previousValue = 0;
+    
+    while((s[0] = c = getch()) == ' ' || c == '\t')
+    s[1] = '\0';
+    
+    if (!isdigit(c) && c != '.') {
+        if (c == '-') {
+            previousValue = c;
+            s[++i] = c = getch();
+            if (isdigit(c))
+                while(isdigit(s[++i] = c = getch()));
+            else {
+                ungetch(c);
+                return previousValue;
+            }
+        } else {
+            return c;
+        }
+    }
+    
+    if (isdigit(c))
+        while(isdigit(s[++i] = c = getch()));
         
+    if(c == '.')
+        while(isdigit(s[++i] = c = getch()));
+        
+    s[i] = '\0';
+    if (c != EOF) {
+        ungetch(c);
+    }
+        
+    return NUMBER;
+}
 
+char buf[BUFSIZE];
+int bufp = 0;
 
+int getch(void) {
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
 
 
 
