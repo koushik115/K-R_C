24d719954e3113f9a79b29337305851584bbde46
Koushik <kaushikbhat115@gmail.com>
2026-02-27 07:29:07 +0530

    Add command-line find-like C utility
    Add commandLineArgs executable and commandLineArgs.c source. The C program implements a simple grep-like utility that reads stdin and searches for a pattern, supporting -x (invert match) and -n (print line numbers) options, prints usage on invalid input, and returns the count of matching lines. Includes a getLine helper and some commented example code/snippets.

---
+++ commandLineArgs.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLINE	1000
+
+int getLine (char *line, int max);
+
+int main (int argc, char *argv[]) {
+	/*
+	for (int i = 0; i < argc; i++)
+		printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+	printf("\n");
+	*/
+	/*
+	while (--argc > 0)
+		printf("%s%s", *++argv, (argc > 1) ? " " : "");
+	printf("\n");
+	*/
+	/*
+	char line[MAXLINE];
+	int found = 0;
+
+	if (argc != 2)
+		printf("Usage: find pattern\n");
+	else {
+		while (getLine(line, MAXLINE) > 0) {
+			if (strstr(line, argv[1]) != NULL) {
+				printf("%s", line);
+				found++;
+			}
+		}
+	}
+	return found;
+	*/
+
+	char line[MAXLINE];
+	long lineno = 0;
+	int c, except = 0, number = 0, found = 0;
+
+	while(--argc > 0 && (*++argv)[0] == '-') {
+		while(c = *++argv[0]) {
+			switch(c) {
+			case 'x':
+				except = 1;
+				break;
+
+			case 'n':
+				number = 1;
+				break;
+
+			default:
+				printf("find: illegal option %c\n", c);
+				argc = 0;
+				found = -1;
+				break;
+			}
+		}
+	}
+
+	if(argc != 1) {
+		printf("Usage: find -x -n pattern\n");
+	} else {
+		while(getLine(line, MAXLINE) > 0)  {
+			lineno++;
+			if((strstr(line, *argv) != NULL) != except) {
+				if(number)
+					printf("%ld:", lineno);
+				printf("%s", line);
+				found++;
+			}
+		}
+	}
+
+	return found;
+}
+
+int getLine (char *line, int max) {
+	int i, c;
+
+	for (i = 0; i < max && (c = getchar()) != EOF && c != '\n'; i++)
+		line[i] = c;
+
+	if (c == '\n')
+		line[i++] = c;
+
+	line[i] = '\0';
+	return i;
+}
